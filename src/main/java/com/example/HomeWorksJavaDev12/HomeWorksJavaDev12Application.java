0759bee458b28b033ca623e5a8f185153b93ce9c
Sonar <Sonarchiko@gmail.com>
2023-03-21 15:19:59 +0200

    first commit

---
+++ src/main/java/com/example/HomeWorksJavaDev12/HomeWorksJavaDev12Application.java
@@ -0,0 +1,13 @@
+package com.example.HomeWorksJavaDev12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorksJavaDev12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorksJavaDev12Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/HomeWorksJavaDev12/entity/Note.java
@@ -0,0 +1,10 @@
+package com.example.HomeWorksJavaDev12.entity;
+
+import lombok.Data;
+
+@Data
+public class Note {
+    private long id;
+    private String title;
+    private String content;
+}
